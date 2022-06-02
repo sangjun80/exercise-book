fbb85875a9dfbcfd7ca9a8e963e356a3d4a3ee1d
leesj <tkdrmsdl80@naver.com>
2022-06-02 12:49:47 +0900

    0602 쉅

--- leesj11/leesj11/leesj11.cpp
+++ leesj11/leesj11/leesj11.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 int main(void)
 {
+	int Size = 10;
+	int iter = 0;
+	int* Vector = new int[Size];
 
+
+	for (int i = 0; i < Size; ++i)
+		Vector[i] = i;
+
+	for (int i = 0; i < Size; ++i)
+		cout << Vector[i] << endl;
 
 	return 0;
 }
@@ -12,7 +21,7 @@ int main(void)
 
 
 
-//배열 (첫 시간)
+//배열
 /*
 장점
 직접 접근 가능
@@ -55,3 +64,41 @@ int main(void)
 	return 0;
 }
 */
+/*
+배열은 0이 아닌 값으로만 초기화가 가능하다.
+배열은 상수값으로만 초기화가 가능하다.
+아래와 같이 사용한다면 위 조건은 무시할 수 있다.
+
+int Size = 0;
+int iter = 0;
+int* Vector = new int[Size];
+
+Vector[iter]
+
+/*
+//배열을 만들 때 컴퓨터가 인지하는건 2진수라서 2진수로 표기하는게 좋음(ex/ 50을 쓰려면 64로)
+
+	char n = 16;
+
+	printf_s("%d", n >> 1);
+
+	int Vector[64];
+
+
+*/
+
+
+//	0 0000000 0
+//	0 0000001 1
+//	0 0000010 2
+//  0 0000011 3
+//	0 0000100 4
+//	0 0000101 5
+//	0 0000110 6
+//	0 0001000 
+//	0 0001001 
+//	.
+//	.
+//	.
+//	0 1111111 127 
+//	1 0000000 -1
